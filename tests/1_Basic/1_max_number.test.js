6451171884caa83901c02c0a2d27fcb042e48f78
Narayan Gautam <ng4111894@gmail.com>
2024-09-29 00:54:54 +0545

    write test for max_number function

---
+++ tests/1_Basic/1_max_number.test.js
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+import { max_number } from '../../src/1_Basic/1_max_number';
+
+describe('max_number', () => {
+  it('should return the first argument if it is greator', () =>
+    expect(max_number(6, 2)).toBe(6));
+
+  it('should return the second argument if it is greator', () =>
+    expect(max_number(2, 6)).toBe(6));
+
+  it('should return the first argument both arguments are equal', () =>
+    expect(max_number(6, 6)).toBe(6));
+});

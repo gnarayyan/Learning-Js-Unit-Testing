9839595c54fa754b46f65ea678d9bc5b93e51ef3
Narayan Gautam <ng4111894@gmail.com>
2024-09-29 01:04:48 +0545

    Correct spelling mistake

--- tests/1_Basic/1_max_number.test.js
+++ tests/1_Basic/1_max_number.test.js
@@ -2,10 +2,10 @@ import { describe, expect, it } from 'vitest';
 import { max_number } from '../../src/1_Basic/1_max_number';
 
 describe('max_number', () => {
-  it('should return the first argument if it is greator', () =>
+  it('should return the first argument if it is greater', () =>
     expect(max_number(6, 2)).toBe(6));
 
-  it('should return the second argument if it is greator', () =>
+  it('should return the second argument if it is greater', () =>
     expect(max_number(2, 6)).toBe(6));
 
   it('should return the first argument both arguments are equal', () =>

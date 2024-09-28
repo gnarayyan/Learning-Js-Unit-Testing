cf616a250cd48e805ba89863db3e372fc13e7cfe
Narayan Gautam <ng4111894@gmail.com>
2024-09-29 00:56:17 +0545

    write fizzBuzz program

--- src/1_Basic/1_max_number.js
+++ src/1_Basic/1_max_number.js
@@ -1,14 +1,1 @@
 export const max_number = (a, b) => (a > b ? a : b);
-
-export const fizzBuzz = (n) => {
-  const divBy3 = n % 3 === 0;
-  const divBy5 = n % 5 === 0;
-
-  return divBy3 && divBy5
-    ? 'FizzBuzz'
-    : divBy3
-    ? 'Fizz'
-    : divBy5
-    ? 'Buzz'
-    : n.toString();
-};
